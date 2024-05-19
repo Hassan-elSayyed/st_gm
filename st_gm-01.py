01737856002733662b424b176cea12b360367140
Hassan <134644043+Hassan-elSayyed@users.noreply.github.com>
2024-05-19 19:33:49 +0400

    Update st_gm-01.py

--- st_gm-01.py
+++ st_gm-01.py
@@ -1,13 +1,13 @@
 import streamlit as st 
-import plotly.offline as py
+# import plotly.offline as py
 # py.init_notebook_mode(connected=True)
-import plotly.graph_objs as go
+# import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 import plotly.express as px
-from plotly.figure_factory import create_table
+# from plotly.figure_factory import create_table
 
 subject = 'gapminder'
 # df = px.data.gapminder()
