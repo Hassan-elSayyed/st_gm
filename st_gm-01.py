24049aa4a4899cc043a646b1559ccc2113710407
Hassan <134644043+Hassan-elSayyed@users.noreply.github.com>
2024-05-20 01:49:29 +0400

    Update st_gm-01.py

--- st_gm-01.py
+++ st_gm-01.py
@@ -1,26 +1,10 @@
 import streamlit as st 
-# import plotly.offline as py
-# py.init_notebook_mode(connected=True)
-# import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
 
 import plotly.express as px
-# from plotly.figure_factory import create_table
 
 subject = 'gapminder'
-# df = px.data.gapminder()
-# table = create_table(df.head(10))
-# py.iplot(table)
-# print(table)
-
-# @st.cache_data
-# def lifespan_gdp():
-#     labels = dict(pop='Population', gdpPercap='DGP per Capita', lifeExp='Life Expectancy')
-#     px.scatter(gm_df, x='gdpPercap', y='lifeExp', color='continent', size='pop', size_max=60, hover_name='country', animation_frame='year', animation_group='country', log_x=True, range_x=[100, 100000], range_y=[25, 90], labels=labels)
-# gm = lifespan_gdp()
-# st.write(gm)
     
 st.header('Lifespan correlation with Gross Domestic Product')
 st.markdown('GDP per capita increases the life expectancy at birth through increasing economic growth and development in a country and thus leads to the prolongation of longevity (Loichinger & Weber, 2019, p.)')
